68f0c5c37f6b1a9b606a79682d8e91de159db371
TeoGonz <Matt.Grisa@Gmail.com>
2022-09-14 20:57:21 -0500

    10. Creando tu primera entidad

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+//MODULES: 
+//Products:
+import { ProductsController } from './controllers/products.controller';
+import { ProductsService } from './services/products.service';
+import { Product } from './entities/product.entity';
 //Controllers
 import { CategoriesController } from './controllers/categories.controller';
-import { ProductsController } from './controllers/products.controller';
 import { BrandsController } from './controllers/brands.controller';
 //services
-import { ProductsService } from './services/products.service';
 import { BrandsService } from './services/brands.service';
 import { CategoriesService } from './services/categories.service';
 
 @Module({
+  imports: [ TypeOrmModule.forFeature([Product]) ],
   controllers: [CategoriesController, ProductsController, BrandsController],
   providers: [ProductsService, BrandsService, CategoriesService],
   exports: [ProductsService]
