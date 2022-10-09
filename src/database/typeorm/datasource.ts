e771cb350a5d8d2f377f780e108605975856e721
TeoGonz <Matt.Grisa@Gmail.com>
2022-10-09 17:12:35 -0500

    15. Extra - DataSource with env

--- src/database/typeorm/datasource.ts
+++ src/database/typeorm/datasource.ts
@@ -1,14 +1,20 @@
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+
+//env config
+config();
+const configService = new ConfigService();
 
 export const connectionSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: '123456',
-  database: 'store_db',
+  host: configService.get('PG_HOST'),
+  port: configService.get('PG_PORT'),
+  username: configService.get('PG_USER'),
+  password: configService.get('PG_PASSWORD'),
+  database: configService.get('PG_DATABASE'),
   logging: true,
   synchronize: false, 
   entities: ['src/**/*.entity.ts'],
-  migrations: ['src/database/typeorm/migrations/*.ts'],
+  migrations: [configService.get('MIGRATIONS_DIR')],
 });
