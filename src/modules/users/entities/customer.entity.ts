d67f70f0090813c1392163b4f8c0d4f9f15ea7cf
TeoGonz <Matt.Grisa@Gmail.com>
2022-10-18 21:24:32 -0500

    18. Relaciones 1:1: Entidades y módulo

--- src/modules/users/entities/customer.entity.ts
+++ src/modules/users/entities/customer.entity.ts
@@ -1,7 +1,37 @@
+import { 
+  Column, 
+  CreateDateColumn, 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  UpdateDateColumn 
+} from "typeorm";
+
+@Entity()
 export class Customer {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({type: 'varchar', length: 255})
   name: string;
+
+  @Column({type: 'varchar', length: 255})
   lastname: string;
+
+  @Column({type: 'varchar'})
   email: string;
+
+  @Column({type: 'varchar'})
   phone: string;
+
+  @CreateDateColumn({
+    type: "timestamptz", //Nos permite ajustar la zona horaria automáticamente
+    default: () => 'CURRENT_TIMESTAMP', //Nos permite registrar el dato automáticamente
+  }) 
+  createdAt:Date;
+
+  @UpdateDateColumn({
+    type: "timestamptz", //Nos permite ajustar la zona horaria automáticamente
+    default: () => 'CURRENT_TIMESTAMP', //Nos permite registrar el dato automáticamente
+  }) 
+  updatedAt:Date;
 }
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,6 @@
+//Module settings
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 //Controllers
 import { UsersController } from './controllers/users.controller';
 import { CustomersController } from './controllers/customers.controller';
@@ -7,9 +9,14 @@ import { UsersService } from './services/user.service';
 import { CustomerService } from './services/customers.service';
 //Modules
 import { ProductsModule } from './../products/products.module';
+import { User } from './entities/user.entity';
+import { Customer } from './entities/customer.entity';
 
 @Module({
-  imports: [ ProductsModule], 
+  imports: [ 
+    ProductsModule, 
+    TypeOrmModule.forFeature([User, Customer])
+  ], 
   controllers: [UsersController, CustomersController], 
   providers: [UsersService, CustomerService],
 })
