8df2fd997a89b00fa7a87511d20a71568632da62
TeoGonz <Matt.Grisa@Gmail.com>
2023-01-10 21:43:50 -0500

    11. Conectando Passport con Jwt

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { UsersService } from 'src/modules/users/services/user.service';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt'
+
+import { User } from 'src/modules/users/entities/user.entity';
+import { UsersService } from 'src/modules/users/services/user.service';
+import { PayloadToken } from '../model/token.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) { }
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService
+  ) { }
 
   async userAuth(email: string, pass: string) {
     const user = await this.userService.findByEmail(email);
@@ -13,5 +20,13 @@ export class AuthService {
       if (match) return user;
     }
     return null;
+  }
+
+  generateJwt(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user
+    }
   }
 }
